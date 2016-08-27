1f412f22aeea60ec85f013020b3c4d2ff4c307d7
Jack Weiss <jacobaweiss@gmail.com>
2016-08-27 14:00:51 -0400

    Adds some aliases

---
+++ aliases.zsh
@@ -0,0 +1,17 @@
+# navigation
+alias up='cd ..'
+alias upp='cd ../..'
+alias cdw='cd ~/workspace'
+
+# git
+alias gs='git status'
+alias gls='git log --stat'
+alias gap='git add --patch'
+alias gcm='git commit -m'
+alias gcb='git checkout -b'
+
+#rails
+alias bx='bundle exec'
+
+alias gpg='gpg2'
+alias vim='nvim'
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux/conf ~/.tmux.conf
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 
 if [ -e ~/.config/nvim ]; then
   echo "nvim already symlinked, skipping..."
